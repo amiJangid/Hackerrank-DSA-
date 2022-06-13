b05198a874dc08467889bbe3acd6b6e00322f468
Ami Jangid <91718180+amiJangid@users.noreply.github.com>
2022-06-13 08:36:29 +0530

    Update 6_Array-Manipulation.java

--- 6_Array-Manipulation.java
+++ 6_Array-Manipulation.java
@@ -1,43 +1,32 @@
-import java.util.*;
 import java.io.*;
-public class Solution {
-
-    static long arrayManipulation(int n, int[][] queries) {
-
-        long outputArray[] = new long[n + 2];
-        for (int i = 0; i < queries.length; i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            int k = queries[i][2];
-            outputArray[a] += k;
-            outputArray[b+1] -= k;
+import java.util.*;
+public class Solution 
+{
+    public static void main(String gg[])
+    {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int q=scn.nextInt();
+        ArrayList<Integer> []queries=new ArrayList[3];
+        long ans[]=new long[n+2];
+        for(int i=0;i<q;i++)
+        {
+            int a=scn.nextInt();
+            int b=scn.nextInt();
+            int k=scn.nextInt();
+            ans[a]+=k;
+            ans[b+1]+=-k;  
         }
-        long max = getMax(outputArray);
-        return max;
-    }
-    private static long getMax(long[] inputArray) {
-        long max = Long.MIN_VALUE;
-        long sum = 0;
-        for (int i = 0; i < inputArray.length; i++) {
-            sum += inputArray[i];
-            max = Math.max(max, sum);
+        //Now apply prefix sum algorithm
+        for(int i=1;i<n+2;i++)
+        {
+            ans[i]=ans[i]+ans[i-1];
         }
-        return max;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int queries[][] = new int[m][3];
-
-        for (int i = 0; i < m; i++) {
-            queries[i][0] = sc.nextInt();
-            queries[i][1] = sc.nextInt();
-            queries[i][2] = sc.nextInt();
+        long max=Integer.MIN_VALUE;
+        for(int i=0;i<ans.length;i++)
+        {
+            max=Math.max(max,ans[i]);
         }
-        long max = arrayManipulation(n, queries);
         System.out.println(max);
-        sc.close();
     }
 }
