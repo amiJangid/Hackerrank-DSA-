3ddd750462139444e13842808b31f198d37a3686
Ami Jangid <91718180+dreamer-girl@users.noreply.github.com>
2022-09-25 21:39:35 +0530

    Update 13_PlusMinus.java

--- 13_PlusMinus.java
+++ 13_PlusMinus.java
@@ -1,28 +1,33 @@
 // TC:O(N)
-public static void plusMinus(List<Integer> arr) {
-        
-     int n=arr.size();
-     int plus=0;
-     int minus=0;
-     int zero=0;
-     for(int i=0;i<n;i++)
-     {
-         if(arr.get(i)>0)
-         {
-           plus++;  
-         }
-         else if(arr.get(i)<0)
-         {
-           minus++;  
-         }
-         else
-         {
-           zero++;  
-         }
-     }
-     System.out.printf("%.6f\n",(float)plus/(float)n);
-     System.out.printf("%.6f\n",(float)minus/(float)n);
-     System.out.printf("%.6f\n",(float)zero/(float)n);
-    }
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double numInputs = scan.nextDouble();
+        int posInputs = 0;
+        int negInputs = 0;
+        int zeroInputs = 0;
+
+        for(int i = 0; i < numInputs; i++){
+            int tmp = scan.nextInt();
+            
+            if (tmp > 0){
+                posInputs++;
+            }
+            else if(tmp < 0){
+                negInputs++;
+            }
+            else{
+                zeroInputs++;
+            }
+        }
+        scan.close();
+        
+        // Print result:
+        System.out.println(posInputs / numInputs);
+        System.out.println(negInputs / numInputs);
+        System.out.println(zeroInputs / numInputs);
+    }
 }
