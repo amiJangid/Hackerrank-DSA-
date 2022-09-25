398d7b7381e25ebf3768fd567daf08951792b632
Ami Jangid <91718180+dreamer-girl@users.noreply.github.com>
2022-09-25 20:57:04 +0530

    Update 12_Diagonal Difference.java

--- 12_Diagonal Difference.java
+++ 12_Diagonal Difference.java
@@ -3,23 +3,41 @@ with O(n) complexity:
 We need to calculate the sums across the two diagonals of a square matrix. Along the first diagonal of the matrix, 
 row index = column index. The second diagonal is at column  for each row . Loop through the rows, summing both values as  increments.
 */
-int primaryDigonal = 0, secondDigonal = 0;
-    int k = 0, arrCount = arr_columns - 1;
-    for(int i = 0; i< arr_columns; ++i) {
-        primaryDigonal += arr[i][i];
-        secondDigonal += arr[i][arrCount - k];
-        k++;
+public static int diagonalDifference(List<List<Integer>> arr) {
+        
+    int lsum=0;
+    int rsum=0;
+    
+    int n=arr.size();
+
+    for (int i=0;i<n;i++)
+    {
+        
+            lsum+=arr.get(i).get(n-i-1);
+            
+            rsum+=arr.get(i).get(i); 
+    }   
+        return Math.abs(lsum-rsum);
     }
+   
 // with O(n^2) complexity:
 
-for(int i = 0; i< arr_columns; ++i) {
-        primaryDigonal += arr[i][i];
-}
-
-for(int i = 0; i < arr_columns; ++i) {
-        for(int j = arr_rows - 1; j >= 0; --j) {
-            secondDigonal += arr[i][j];
-             i++;
-             continue;
-         }
-     }
+public static int diagonalDifference(List<List<Integer>> arr) {
+    
+    int lsum=0;
+    int rsum=0;
+    
+    int n=arr.size();
+    int s=n-1;
+    for (int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j) lsum+=arr.get(i).get(j);
+            if(i+j==s)
+            rsum+=arr.get(i).get(j);
+        }
+        
+    }   
+        return Math.abs(lsum-rsum);
+    }
